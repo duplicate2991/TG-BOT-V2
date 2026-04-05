279f48b39bd43b6043a9d98c1894e5fc8fe337d8
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-04-05 17:21:58 +0530

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,9 +1,17 @@
+const { InlineKeyboard } = require("grammy");
+
+const kb = new InlineKeyboard()
+  .text("A", "a")
+  .text("B", "b")
+  .row()
+  .text("C", "c");
+
 module.exports = {
   name: "ping",
   aliases: ["p"],
 
   execute(ctx) {
     ctx.session.count++;
-    ctx.reply(`🏓 Pong (${ctx.session.count})`);
+    ctx.reply(`🏓 Pong (${ctx.session.count})`, { reply_markup: kb });
   }
 };
