4f6a62c48f9cbef7f4b20aa52aa983ba803d3a5c
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-04-06 11:28:11 +0530

    Update start.js

--- commands/general/start.js
+++ commands/general/start.js
@@ -2,11 +2,19 @@ module.exports = {
   name: "start",
   description: "Start bot",
 
-  execute(ctx, args) {
+  async execute(ctx, args, bot) {
     if(!args || args.length < 1){
       ctx.reply("👋 Welcome!");
     } else {
-      ctx.reply(`👋 Welcome!, Referred By: ${args[0]}.userId`);
+      if(!RFCode[args[0]]){
+        return ctx.reply(`❌| Invalid or expired Referral Code Provided..`);
+      }else if(!RFCode[args[0]].ttl || isNaN(RFCode[args[0]].ttl) || RFCode[args[0]].ttl < Date.now()){
+        return ctx.reply("⌛| Your Referal Code is expired. please create new one");
+      }
+      const RFUser = await bot.api.getChat(RFCode[args[0]].createdBy);
+      if(!RFUser){ return ctx.reply("⚠️ | Referal creator is not a valid user"); }
+      
+      ctx.reply(`👋 Welcome!, Referred By: ${RFUser}`);
     };
   }
 };
