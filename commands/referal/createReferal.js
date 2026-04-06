3be4f266086253316732829acdb1f4bb0c57996c
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-04-06 10:36:15 +0530

    Update createReferal.js

--- commands/referal/createReferal.js
+++ commands/referal/createReferal.js
@@ -1,1 +1,17 @@
+function randomString(length = 6) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 
+// example: aB3xY9
+module.exports = {
+  name: "createReferal",
+  execute(ctx) {
+    let code = randomString(); 
+    ctx.reply(`Code created: ${code}, Created By: @${ctx.from.username}`);
+  }
+};
