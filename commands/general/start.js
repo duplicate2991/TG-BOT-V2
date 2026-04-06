9d0c1b7b8ada457de238c978202b281b4a169870
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2026-04-06 13:09:05 +0530

    Update start.js

--- commands/general/start.js
+++ commands/general/start.js
@@ -10,6 +10,8 @@ module.exports = {
         return ctx.reply(`❌| Invalid or expired Referral Code Provided..`);
       }else if(!RFCode[args[0]].ttl || isNaN(RFCode[args[0]].ttl) || RFCode[args[0]].ttl < Date.now()){
         return ctx.reply("⌛| Your Referal Code is expired. please create new one");
+      }else if(RFCode[args[0]].createdBy === ctx.from.id){
+        return ctx.reply("👾 | You cannot use your own Referral Code. Sent it to your friends!");
       }
       const RFUser = await bot.api.getChat(RFCode[args[0]].createdBy);
       if(!RFUser){ return ctx.reply("⚠️ | Referal creator is not a valid user"); }
